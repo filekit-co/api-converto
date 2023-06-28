907c4bc07ccd80ab15716d043bbc509ac376e6c3
minkj1992 <minkj1992@gmail.com>
2023-06-28 22:34:30 +0900

    fix: cors http header

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 import os
 
 import uvicorn
-from api import pdf_kit, to_doc, to_docx, to_pdf
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+
+from api import pdf_kit, to_doc, to_docx, to_pdf
 
 app = FastAPI(title='Converto pdf api server')
 
@@ -27,6 +28,16 @@ routers = [
 for router in routers:
     app.include_router(router)
 
+
+@app.middleware("http")
+async def add_custom_header(request: Request, call_next):
+    # after process
+    response = await call_next(request)
+    # 기존 응답 헤더에 'Access-Control-Expose-Headers' 헤더를 추가합니다.
+    # CORS 정책으로 인해 Content-Disposition를 sveltekit이 못가져올 경우가 존재합니다.
+    # Content-Disposition는 파일 이름을 전달하기 위해 필요합니다.
+    response.headers["Access-Control-Expose-Headers"] = "Content-Disposition"
+    return response
 
 
 if __name__ == "__main__":
